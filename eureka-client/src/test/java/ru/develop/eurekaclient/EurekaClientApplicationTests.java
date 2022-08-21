b0c18ae30ea6d863fba0c45374c7decdac2ff4da
Николай Шведов <shved.nikolay.18@mail.ru>
2022-08-21 15:23:03 +0300

    lesson 2: #add the Eureka Client module

---
+++ eureka-client/src/test/java/ru/develop/eurekaclient/EurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.develop.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
