8753d8559789e2e01ee49c8fc9bc98b08ae28f99
Николай Шведов <shved.nikolay.18@mail.ru>
2022-08-21 15:23:03 +0300

    esson 2: #add the Eureka Client module

---
+++ eureka-client/src/main/java/ru/develop/eurekaclient/controller/TestController.java
@@ -0,0 +1,13 @@
+package ru.develop.eurekaclient.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
