eff9becebb2563143c3fb0ac0cccf9187b3e284d
Николай Шведов <shved.nikolay.18@mail.ru>
2022-08-22 15:49:50 +0300

    lesson 3: #add API Gateway

---
+++ api-gateway/src/test/java/ru/develop/eureka/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.develop.eureka.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- eureka-client/src/main/java/ru/develop/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/ru/develop/eurekaclient/controller/TestController.java
@@ -1,9 +1,11 @@
 package ru.develop.eurekaclient.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/main")
 public class TestController {
 
     @GetMapping("/test")
