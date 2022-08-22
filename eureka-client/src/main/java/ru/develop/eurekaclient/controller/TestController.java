cfee35a8aaa7e1513c8fd2a644f3eba9cd0d792a
Николай Шведов <shved.nikolay.18@mail.ru>
2022-08-22 16:27:43 +0300

    lesson 4: #add Load balancer in API Gateway

--- eureka-client/src/main/java/ru/develop/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/ru/develop/eurekaclient/controller/TestController.java
@@ -1,5 +1,6 @@
 package ru.develop.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main")
 public class TestController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
     @GetMapping("/test")
     public String test() {
-        return "test";
+        return id;
     }
 }
